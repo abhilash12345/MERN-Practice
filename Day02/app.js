e4de23184e62c6156feb00be92dea9d8a3a31928
abhilash <kumarsinghabhilash@gmail.com>
2025-06-14 16:08:25 +0530

    day01
    we understood function basic

---
+++ Day02/app.js
@@ -0,0 +1,11 @@
+// Callbacks
+
+// --> function which is called by the handler
+
+const print = (txt) =>{
+    console.log("---------------")
+    console.log("----",txt)
+    console.log("---------------")
+
+}
+print("Hello")
