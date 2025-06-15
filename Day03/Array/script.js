fecab725dfca352c4edfc8ece450f420f0ce71be
abhilash <kumarsinghabhilash@gmail.com>
2025-06-15 23:45:56 +0530

    array
    learn diff methods of array

---
+++ Day03/Array/script.js
@@ -0,0 +1,59 @@
+// const state = {
+//     0:"delhi",
+//     1:"UP",
+//     2:"Punjab",
+//     3:"Haryana",
+//     length:4
+// }
+
+// console.log(state)
+
+
+
+
+// const state2 = ["delhi","UP","punjab","haryana"]
+// console.log(state2)
+
+// --------------------------------------------------------------------------
+
+// const fruits = Array("mango","banana")
+// console.log(Array.isArray(fruits))
+// console.log(fruits[1])
+
+
+// ---------------------------------------------------------------------------------
+
+// const fruits = Array("mango","banana")
+// fruits.push("watermelon")
+
+// console.log(fruits)
+
+// fruits[1] = "cherry" // index that we are using is less than the length of the array
+// console.log(fruits)
+
+// const state2 = ["delhi","UP","punjab","haryana"]
+
+// state2.pop()
+// console.log(state2)
+// state2.pop()
+// console.log(state2)
+
+
+// console.log(state2)
+
+// // state2.unshift("kerala") // it will add element at the first place of array
+// state2.shift()  // remov from the first element
+
+// console.log(state2)
+
+//----------------------------------------------------------------
+
+//splice
+
+const state2 = ["delhi","UP","punjab","haryana"]
+
+console.log(state2)
+
+state2.splice(1,1)
+
+console.log(state2)
