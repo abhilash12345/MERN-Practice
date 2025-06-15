d378d1185f5504b3508e6831cc409b804ed57da5
Abhilash <kumarsinghabhilash@gmail.com>
2025-06-16 00:29:53 +0530

    array2

--- Day03/Array/script.js
+++ Day03/Array/script.js
@@ -50,10 +50,82 @@
 
 //splice
 
-const state2 = ["delhi","UP","punjab","haryana"]
+// const state2 = ["delhi","UP","punjab","haryana"]
 
-console.log(state2)
+// console.log(state2)
 
-state2.splice(1,1)
+// state2.splice(1,1)
 
-console.log(state2)
+// console.log(state2)
+
+//---------------------------------------------------------------------------------------------
+
+// const car = {
+//     brand :"TATA",
+//     model : "Safari",
+//     year: 2025
+
+// }
+
+// for(let i in car){  // because we have object .keys and vallue and entrie sas well
+//     console.log("Key=",i, 'value=',car[i])
+// }
+
+// const keyValuePairs = Object.entries(car)
+// // console.log(keyValuePairs)
+
+// // for (let i = 0 ;i <keyValuePairs.length; i++){
+// //     console.log(keyValuePairs[i][0],keyValuePairs[i][1])
+// // }
+// const arr = ["mango","banana","apple"]
+
+// for (let i of arr){ // us eit as per requirement 
+// // it will iterare on value
+// console.log(i)
+// }
+
+
+//------------------------------------------------------------------------------------------
+
+// const laptop = [
+//     {
+
+//         os:"window",
+//         ram:"16GB",
+//         rom:"1TB",
+//         year:"2019"
+//     },
+//     {
+//         os:"Macos",
+//         ram:"16GB",
+//         rom:"512GB",
+//         year:"2022"
+//     }
+// ]
+
+// console.log(laptop)
+
+//------------------------------
+
+// const Arjun = {
+//     name:"Abhilash Singh",
+//     age:"20",
+//     city:"Delhi",
+//     hobbies: ["playing badminton","walking","game development","painting"],
+//     marks:{
+//         english:40,
+//         evs:42,
+//         physics:99
+
+//     }
+// }
+
+
+// console.log(Arjun)
+
+
+
+
+
+
+
