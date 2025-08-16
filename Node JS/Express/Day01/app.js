84e8ca01e5201cbc9b50ccd02a808e461eab2fe9
Abhilash <kumarsinghabhilash@gmail.com>
2025-08-16 23:58:03 +0530

    patch and delete using express.js

--- Node JS/Express/Day01/app.js
+++ Node JS/Express/Day01/app.js
@@ -1,5 +1,6 @@
-const express = require("express"); // requiire ->> internal , it is user-define , is it node-module.
+const express = require("express"); // requiire ->> internal , it is user-defined , is it node-module.
 const { myReadFile, mySaveFile } = require("./utils/file_helper");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -13,8 +14,6 @@ app.use((req, res, next) => {
 app.get("/api/v1/products", async (req, res) => {
   const productArr = await myReadFile("./data.json");
   res.json({
-    isSuccess: true,
-    message: "(GET) working...",
     data: { products: productArr },
   });
 });
@@ -22,6 +21,9 @@ app.get("/api/v1/products", async (req, res) => {
 app.post("/api/v1/products", async (req, res) => {
   const data = req.body;
   console.log(data);
+  newId = uuidv4();
+  data.id = newId;
+
   const oldArr = await myReadFile("./data.json");
   oldArr.push(data);
   await mySaveFile("./data.json", oldArr);
@@ -32,6 +34,63 @@ app.post("/api/v1/products", async (req, res) => {
   });
 });
 
+app.patch("/api/v1/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const data = req.body;
+  const products = await myReadFile("./data.json");
+
+  const idx = products.findIndex((elem) => {
+    return elem.id === productId;
+  });
+  if (idx === -1) {
+    res.status(400);
+    res.status({
+      isSuccess: false,
+      message: "Invalid Product Id",
+    });
+    return;
+  }
+
+  const oldObj = products[idx];
+  products[idx] = { ...oldObj, ...data };
+  mySaveFile("./data.json", products);
+  res.status(200);
+  res.json({
+    isSuccess: true,
+    message: "product updated",
+    data: {
+      product: products[idx],
+    },
+  });
+});
+
+app.delete("/api/v1/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+
+  const products = await myReadFile("./data.json");
+
+  const idx = products.findIndex((elem) => {
+    return elem.id === productId;
+  });
+  if (idx === -1) {
+    res.status(400);
+    res.json({
+      isSuccess: false,
+      message: "Invalid Product Id",
+    });
+    return;
+  }
+
+  products.splice(idx, 1);
+
+  mySaveFile("./data.json", products);
+  res.status(204);
+  res.json({
+    isSuccess: true,
+    message: "product deleted",
+    data: {},
+  });
+});
 app.listen(3900, () => {
   console.log("--------------------server started-----------------");
 });
